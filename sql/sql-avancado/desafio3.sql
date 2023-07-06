5a93acc61a80e1ede76cdb26fafe219da1f56878
Karen Caitano <karen.ferla@eemovel.com>
2023-07-05 21:50:36 -0300

    Add new file desafio3.sql

---
+++ sql/sql-avancado/desafio3.sql
@@ -0,0 +1,42 @@
+---1 - Listar o nome do empregado e o nome do respectivo departamento para todos os empregados que não estão alocados em projetos, resolver com:
+---a) NOT EXISTS
+SELECT nom_empregado, 
+       nom_depto
+FROM empregado e
+JOIN departamento d on d.cod_depto = e.cod_depto
+WHERE NOT EXISTS (SELECT 1 
+FROM alocacao a 
+WHERE a.num_matricula = e.num_matricula)
+
+---b) NOT IN
+SELECT nom_empregado, 
+       nom_depto
+FROM empregado e
+JOIN departamento d on d.cod_depto = e.cod_depto
+WHERE e.num_matricula NOT IN 
+                    (SELECT num_matricula FROM alocacao a)
+
+---c) LEFT JOIN
+SELECT nom_empregado, 
+       nom_depto
+FROM empregado e
+JOIN departamento d on d.cod_depto = e.cod_depto
+LEFT JOIN alocacao a on a.num_matricula = e.num_matricula 
+WHERE a.num_matricula is null
+
+
+---2 - Listar o empregado, o número de horas e o projeto cuja alocação de horas no projeto é maior do que a média de alocação do referido projeto.
+SELECT a.cod_projeto,
+       nom_empregado,  
+       nom_projeto, 
+       media, 
+       SUM(num_horas) AS qtd_horas
+FROM empregado e
+JOIN alocacao a on a.num_matricula = e.num_matricula
+JOIN projeto p on p.cod_projeto = a.cod_projeto
+JOIN (SELECT cod_projeto, AVG (num_horas) media
+FROM alocacao a 
+GROUP BY cod_projeto) a_media ON a.cod_projeto = a_media.cod_projeto
+GROUP BY a.cod_projeto, nom_empregado, nom_projeto, media
+HAVING SUM(num_horas) > media
+
